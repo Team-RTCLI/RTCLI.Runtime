b3b9bbb70f7d054bc86e3ee7c269a6eb11ff65c9
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2020-10-16 13:47:59 +0800

    Add OpCode struct & static OpCodes Declaration

--- RTCLI/CIL/OpCodes.h
+++ RTCLI/CIL/OpCodes.h
@@ -4,14 +4,6 @@
 
 namespace RTCIL::CIL
 {
-    struct OpCode
-    {
-        RTCLI_FORCEINLINE Code Code(void) RTCLI_NOEXCEPT;
-        RTCLI_FORCEINLINE StackBehaviour StackBehaviourPop(void) RTCLI_NOEXCEPT;
-        RTCLI_FORCEINLINE StackBehaviour StackBehaviourPush(void) RTCLI_NOEXCEPT;
-        RTCLI_FORCEINLINE RTCLI::string Name(void) RTCLI_NOEXCEPT;
-    };
-
     enum class OpCodeType : RTCLI::u32 
     {
         Annotation = 0,
@@ -20,6 +12,42 @@ namespace RTCIL::CIL
         Objmodel = 3,
         Prefix = 4,
         Primitive = 5
+    };
+
+    enum class FlowControl : RTCLI::u32 
+    {
+        Branch = 0,
+        Break = 1,
+        Call = 2,
+        Cond_Branch = 3,
+        Meta = 4,
+        Next = 5,
+        Phi = 6,
+        Return = 7,
+        Throw = 8
+    };
+
+    struct OpCode
+    {
+        OpCode() = default;
+        ~OpCode() = default;
+
+        RTCLI_FORCEINLINE RTCLI::string Name(void) const RTCLI_NOEXCEPT;
+        RTCLI_FORCEINLINE RTCLI::usize Size(void) const RTCLI_NOEXCEPT;
+
+        RTCLI_FORCEINLINE Code Code(void) const RTCLI_NOEXCEPT;
+        RTCLI_FORCEINLINE RTCLI::i16 Value(void) const RTCLI_NOEXCEPT;
+
+        RTCLI_FORCEINLINE RTCLI::u8 Op1(void) const RTCLI_NOEXCEPT;
+        RTCLI_FORCEINLINE RTCLI::u8 Op2(void) const RTCLI_NOEXCEPT;
+
+        RTCLI_FORCEINLINE StackBehaviour StackBehaviourPop(void) const RTCLI_NOEXCEPT;
+        RTCLI_FORCEINLINE StackBehaviour StackBehaviourPush(void) const RTCLI_NOEXCEPT;
+
+        RTCLI_FORCEINLINE OpCodeType OpCodeType(void) const RTCLI_NOEXCEPT;
+        RTCLI_FORCEINLINE FlowControl FlowControl(void) const RTCLI_NOEXCEPT;
+
+        bool operator==(const OpCode& rhs) RTCLI_NOEXCEPT;
     };
 
     namespace OpCodes
